e652844cb12074762dc2be3cabb2cb8d98401def
David Lordan <DavidLordan@users.noreply.github.com>
2015-03-20 12:10:56 -0400

    Update app_config.php

--- blog_admin/scripts/app_config.php
+++ blog_admin/scripts/app_config.php
@@ -1,8 +1,8 @@
 <?php
 	
 	define('DATABASE_HOST', 'localhost');
-	define('DATABASE_USERNAME','davelord');
-	define('DATABASE_PASSWORD','AmAvYqArY');
+	define('DATABASE_USERNAME','');
+	define('DATABASE_PASSWORD','');
 	define('DATABASE_NAME','davelord_test_blog');
 	define('MY_TABLE_NAME','test_blog_table');
 	define('MY_HEADER','Location: http://www.daves-data.com/test_blog/')
